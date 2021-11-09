b535d59750d527922138277c607cac6bff75f8f9
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-11-09 22:12:56 +0100

    First Test

---
+++ test/Inbox.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('Web3')
+
+/*class Car{
+    park(){
+        return 'Parked'
+    }
+
+    drive(){
+        return 'vroom'
+    }
+}
+
+let car ;
+
+beforeEach(()=>{
+    car = new Car()
+})
+
+describe('Car Test',()=>{
+    it('can park',()=>{
+        assert.equal(car.park(),'Parked')
+    })
+    it('can drive',()=>{
+        assert.equal(car.drive(),'vroom')
+    })
+})*/
