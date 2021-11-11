c6541f3329c6b8a401e9ce7ef53215484edf247e
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-11-11 22:25:52 +0100

    Upgrade to 0.8.10

---
+++ updated-v0.8.9/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+    'bleak bone sibling void crash art boss panic merit resemble power crouch',
+    'https://rinkeby.infura.io/v3/c9f0a049027845e9a6a5e1c06238cdad'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
